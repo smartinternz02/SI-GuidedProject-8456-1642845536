66db0439db68125513fd08a905c1292516b832ed
Manikandan G <88706529+iammanikandann@users.noreply.github.com>
2022-03-24 17:18:14 +0530

    Add files via upload

---
+++ Nutro-Assistant-main/Nutro-Assistant/SpringApiTest/src/main/java/com/demian/SpringApiTest/ApiCall.java
@@ -0,0 +1,29 @@
+package com.demian.SpringApiTest;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javassist.bytecode.Descriptor.Iterator;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+
+public class ApiCall {
+	
+	
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+			    
+	}
+
+}
